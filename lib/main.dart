39473ecff9ed3965a99721777bec971386013929
Master-Bibash <dokfreak142@gmail.com>
2024-02-12 15:45:38 +0545

    geolocation that shows location

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: GeoLocationScreen(),
+        home: LocationPage(),
       ),
       designSize: Size(375, 812),
     );
